83f5b1661c4725c419f1dc51051e846fa2109d9b
Diogo Miranda <diogosminfor@gmail.com>
2025-01-22 09:57:47 -0300

    configure backend s3 and terraform

--- main.tf
+++ main.tf
@@ -5,8 +5,33 @@ terraform {
       version = "5.84.0"
     }
   }
+  backend "s3" {
+    bucket  = "diogomiranda-iac"
+    region  = "us-east-2"
+    key     = "state/terraform.tfstate"
+    encrypt = true
+
+    profile = "diogomiranda-iac-sso"
+  }
 }
 provider "aws" {
   profile = "diogomiranda-iac-sso"
   region  = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "diogomiranda-iac"
+  force_destroy = true
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags = {
+    IAC = "True"
+  }
+}
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "diogomiranda-iac"
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
